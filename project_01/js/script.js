4249c2e93adc064a70ca88e2b2abad4d7a8bfe31
Caio <caio.student@outlook.com>
2020-07-18 14:18:21 -0300

    ajustes

--- project_01/js/script.js
+++ project_01/js/script.js
@@ -1,4 +1,21 @@
-function teste() {
-  var questão_01 = formTest.a.value;
-  console.log(questão_01);
+var options = [ 0, 0, 0, 0 ];
+
+function teste1(option) {
+  options[0] = option;
+  console.log(options);
+}
+
+function teste2(option) {
+  options[1] = option;
+  console.log(options);
+}
+
+function teste3(option) {
+  options[2] = option;
+  console.log(options);
+}
+
+function teste4(option) {
+  options[3] = option;
+  console.log(options);
 }
