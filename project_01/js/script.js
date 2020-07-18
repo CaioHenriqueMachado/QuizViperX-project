299f1cc34abe70d7524ce7f40ead254accd06f7a
Caio <caio.student@outlook.com>
2020-07-18 15:47:02 -0300

    fazendo mudar de cor

--- project_01/js/script.js
+++ project_01/js/script.js
@@ -1,26 +1,34 @@
 var options = [ 0, 0, 0, 0 ];
 
 function teste1(option) {
-  var models = document.getElementsByClassName('question1');
-  // var model = models[option];
-  models[option].element.classList('selected');
-  console.log(models);
-  console.log(model);
-  options[0] = option;
-  console.log(options);
+  const divs = document.getElementsByClassName('question1');
+  const divSelected = document.getElementsByClassName('question1 selected');
+  console.log(divSelected);
+  divSelected[0].classList.replace('selected', 'unselected');
+
+  // 
+  console.log(divs);
+
+  // for  (let div in divs ){
+  //   console.log(div);
+  // }
+
+//   console.log(divs.forEach());
+  divs[option].classList.add('selected');
+//   options[0] = option;
 }
 
 function teste2(option) {
   options[1] = option;
-  console.log(options);
+
 }
 
 function teste3(option) {
   options[2] = option;
-  console.log(options);
+
 }
 
 function teste4(option) {
   options[3] = option;
-  console.log(options);
+
 }
