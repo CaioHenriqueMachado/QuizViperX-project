f0f7d922e0d7e119b856445b76aa954770c4c8dc
Caio <caio.student@outlook.com>
2020-07-18 16:20:46 -0300

    abstraindo logica

--- project_01/js/script.js
+++ project_01/js/script.js
@@ -1,29 +1,12 @@
 var options = [ 0, 0, 0, 0 ];
 
-function teste1(option) {
-  const divs = document.getElementsByClassName('question1');
-  initial = document.getElementsByClassName('question1 selected');
+function calculator(option, question) {
+  const divs = document.getElementsByClassName('question'+ String(question));
+  initial = document.getElementsByClassName('question'+ String(question) +' selected');
 
   if ( initial.length != 0) {
-    const divSelected = document.getElementsByClassName('question1 selected');
+    const divSelected = document.getElementsByClassName('question'+ String(question) +' selected');
     divSelected[0].classList.replace('selected', 'unselected');
   }
   divs[option].classList.add('selected');
-
-
-}
-
-function teste2(option) {
-  options[1] = option;
-
-}
-
-function teste3(option) {
-  options[2] = option;
-
-}
-
-function teste4(option) {
-  options[3] = option;
-
 }
