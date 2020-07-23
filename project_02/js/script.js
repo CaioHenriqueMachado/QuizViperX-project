94cb90b62deaa1f8d3e0bd3c25a9dfdd4a2cc5a4
Caio <caio.student@outlook.com>
2020-07-22 21:22:50 -0300

    ajuste de personalização

--- project_02/js/script.js
+++ project_02/js/script.js
@@ -1,4 +1,16 @@
 var options = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+var texts = {
+  10: "Não tá ruim, mas também não está bom !!",
+  20: "Pandemia chegou, e o shape aqui está quase indo embora !!",
+  30: "Não se deixe levar pelo tempo de isolamento, da pra melhorar !!",
+  40: "O esforço está bom, mas você consegue melhoras !!",
+  50: "Pandemia chegou, mas o importante é que você não parou !!",
+  60: "A rotina não é a mesma, mas o importante é o esforço para tentar manter !!",
+  70: "Parabéns, está mantendo conseguindo manter o shape do jeito que dá !!",
+  80: "Está se esforçando mesmo nessa crise, é admirável a sua atidude !!",
+  90: "Se manter na atividade em meio a pandemia é para poucos, parabéns !!",
+  100: "A pandemia não abala quem tem mente e corpo forte, parabéns guerreiro(a) !!"
+}
 var points = 0;
 var value = 10;
 
@@ -41,13 +53,15 @@ function finish(){
 }
 
 function modal(){
-  const texto = document.getElementById('h1Error');
-  texto.innerText = `Seu nível marombeiro é de ${points}%`;
+  const title = document.getElementById('h1Error');
+  title.innerText = `Seu nível marombeiro é de ${points}%`;
 
   const img = document.getElementById('imageResult');
   img.src = `./img/result/value${value}.jpg`;
 
+  const text = document.getElementById('textResult');
+  text.innerText = texts[value];
+
   const modal = document.getElementById('modalError');
   modal.classList.toggle('show')
-  
 }
